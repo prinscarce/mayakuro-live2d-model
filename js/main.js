aa31b117b4201fae3c8961096352a25a81f3292f
prinscarce <56539338+prinscarce@users.noreply.github.com>
2022-07-03 15:02:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ class Viewer {
 
             if (this.model) {
                 this.model.position = new PIXI.Point((width * 0.5), (height * 0.5));
-                this.model.scale = new PIXI.Point((this.model.position.x * 0.06), (this.model.position.x * 0.06));
+                this.model.scale = new PIXI.Point((this.model.position.x * 1), (this.model.position.x * 1));
                 this.model.masks.resize(this.app.view.width, this.app.view.height);
 
             }
